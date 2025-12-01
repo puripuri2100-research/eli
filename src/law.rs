d95306fe194f64fb947b3bc2779423d1a3c0aba2
puripuri2100 <puripuri2100@gmail.com>
2025-12-01 17:58:17 +0900

    一旦動く

--- src/law.rs
+++ src/law.rs
@@ -1,8 +1,8 @@
 use crate::eli::{self, EliOntology};
 use anyhow::Result;
-use japanese_law_id::Date;
+pub use japanese_law_id::Date;
 use japanese_law_xml_schema::{
-  article_number::ArticleNumber,
+  article_number::{ArticleNumber, parse_article_number},
   law::LawType,
   utils::{
     Toc, WithNumberArticle, text_from_paragraph_list, toc_list_from_main_provision,
@@ -10,12 +10,18 @@ use japanese_law_xml_schema::{
   },
 };
 pub use oxrdf::Triple;
-use std::collections::HashMap;
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+use std::{cmp::Ordering, collections::HashMap};
+use tracing::{info, trace};
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
 pub struct Law {
   date: Date,
+  // 法令名が無く，法令番号だけの時がある
+  name: Option<String>,
   law_id: String,
+  law_id_text: String,
   patch_id: Option<String>,
   law_type: LawType,
   part_number: Option<ArticleNumber>,
@@ -30,10 +36,18 @@ pub struct Law {
 }
 
 impl Law {
-  pub fn new(date: Date, law_id: String, law_type: LawType) -> Self {
+  pub fn new(
+    date: Date,
+    name: Option<String>,
+    law_id: String,
+    law_id_text: String,
+    law_type: LawType,
+  ) -> Self {
     Self {
       date,
+      name,
       law_id,
+      law_id_text,
       patch_id: None,
       law_type,
       part_number: None,
@@ -46,6 +60,22 @@ impl Law {
       paragraph_text: None,
       egov_link: None,
     }
+  }
+  pub fn set_name(&mut self, name: Option<String>) {
+    self.name = name
+  }
+  pub fn get_name(&self) -> Option<String> {
+    self.name.clone()
+  }
+  pub fn set_law_id(&mut self, id: String, text: String) {
+    self.law_id_text = text;
+    self.law_id = id;
+  }
+  pub fn get_law_id(&self) -> String {
+    self.law_id.clone()
+  }
+  pub fn get_law_id_text(&self) -> String {
+    self.law_id_text.clone()
   }
   pub fn set_patch_id(&mut self, patch_id: String) {
     self.patch_id = Some(patch_id)
@@ -213,7 +243,7 @@ impl eli::Eli for Law {
   /// 日付，法令の種類，法令ID，改正法令ID，条番号，段落番号
   fn eli_uri(&self) -> String {
     format!(
-      "/eli/{:0>4}/{:0>2}/{:0>2}/{}/{}/{}{}",
+      "https://github.com/puripuri2100-research/eli/{:0>4}/{:0>2}/{:0>2}/{}/{}/{}{}",
       self.date.get_ad_year(),
       self.date.get_month(),
       self.date.get_day(),
@@ -236,12 +266,13 @@ impl eli::Eli for Law {
 pub fn egov_xml_parse(
   buf: &[u8],
   date: Date,
+  law_name: Option<String>,
   law_id: String,
-  law_type: LawType,
   patch_id: Option<String>,
 ) -> Result<(HashMap<String, Law>, Vec<Triple>)> {
   let parsed_law = japanese_law_xml_schema::parse_xml(buf)?;
-  let mut law = Law::new(date, law_id, law_type);
+  let law_id_text = parsed_law.law_num;
+  let mut law = Law::new(date, law_name, law_id, law_id_text, parsed_law.law_type);
   if let Some(patch_id) = patch_id {
     law.set_patch_id(patch_id);
   }
@@ -287,4 +318,517 @@ pub fn egov_xml_parse(
     }
   }
   Ok((law_data, v_triple))
+}
+
+fn ord_article_number(a: &ArticleNumber, b: &ArticleNumber) -> Ordering {
+  if a.base_number == b.base_number {
+    for (a_e, b_e) in a.eda_numbers.iter().zip(&b.eda_numbers) {
+      if a_e != b_e {
+        return a_e.cmp(b_e);
+      }
+    }
+    a.eda_numbers.len().cmp(&b.eda_numbers.len())
+  } else {
+    a.base_number.cmp(&b.base_number)
+  }
+}
+
+fn ord_article(a: &Law, b: &Law) -> Ordering {
+  let a_num = &a.article_number;
+  let b_num = &b.article_number;
+  match (a_num, b_num) {
+    (None, Some(_)) => {
+      // Noneは前文の可能性が高い
+      Ordering::Less
+    }
+    (Some(_), None) => Ordering::Greater,
+    (None, None) => {
+      let a_p = &a.paragraph_number;
+      let b_p = &b.paragraph_number;
+      match (a_p, b_p) {
+        (None, None) => Ordering::Equal,
+        (None, Some(_)) => Ordering::Less,
+        (Some(_), None) => Ordering::Greater,
+        (Some(a_pa), Some(b_pa)) => ord_article_number(a_pa, b_pa),
+      }
+    }
+    (Some(a_a), Some(b_a)) => {
+      let o = ord_article_number(a_a, b_a);
+      if o == Ordering::Equal {
+        let a_p = &a.paragraph_number;
+        let b_p = &b.paragraph_number;
+        match (a_p, b_p) {
+          (None, None) => Ordering::Equal,
+          (None, Some(_)) => Ordering::Less,
+          (Some(_), None) => Ordering::Greater,
+          (Some(a_pa), Some(b_pa)) => ord_article_number(a_pa, b_pa),
+        }
+      } else {
+        o
+      }
+    }
+  }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Deserialize, Serialize)]
+pub struct Position {
+  start: usize,
+  end: usize,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
+pub struct FindLawName {
+  position: Position,
+  find_law: Option<Law>,
+  match_string: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
+pub struct Find {
+  /// 参照先
+  to: Law,
+  /// 参照元
+  from: Law,
+  /// fromの中でのテキスト内の位置
+  position: Position,
+}
+
+/// 参照情報を抽出する
+/// - target: 解析対象の法令の情報
+/// - law_map: 法令名や法令IDのテキストをkeyとし，法令全体を表すLawをvalueとするhashmap
+pub fn parse_ref(target: &HashMap<String, Law>, law_map: &HashMap<String, Law>) -> Vec<Find> {
+  // 段落を順番で並べ替える
+  let mut paragraph_list = target
+    .values()
+    .filter(|l| l.paragraph_text.is_some())
+    .cloned()
+    .collect::<Vec<_>>();
+  paragraph_list.sort_by(ord_article);
+
+  // テキストから
+  // - 法令名の出現箇所
+  // - 略称が定義されている箇所
+  // - 同法・同令が出現する箇所
+  // を検索する
+  let mut law_name_list = Vec::new();
+
+  let mut result = Vec::new();
+
+  for paragraph in paragraph_list.iter() {
+    trace!(
+      "[START] parse paragraph {:?} - {:?}",
+      paragraph.article_number_str(),
+      paragraph.paragraph_number_str()
+    );
+    if let Some(text) = &paragraph.paragraph_text {
+      // 正式名称の一覧を持ってテキスト内検索を行う
+      let mut find_law_name_result = find_law_name(text, law_map, &law_name_list);
+
+      // 略称の定義箇所を検索する
+      let find_abb_def_result = find_abb_def(text);
+      // 今までの項で見つかった法令名と，この項で見つかった略称を紐付けていく
+      let mut linked_abb_def_result = find_abb_def_result
+        .iter()
+        .filter_map(|l| linking_abb_and_full_name(l, &find_law_name_result))
+        .collect::<Vec<_>>();
+
+      // 同法に紐付けさせるためのリスト
+      let mut linked_abb_def_result2 = linked_abb_def_result.clone();
+      find_law_name_result.append(&mut linked_abb_def_result2);
+
+      // 同法・同令の出現位置を検索する
+      let find_douhou_result = find_douhou(text);
+      // 今までの項で見つかった法令名と略称の情報と，この項で見つかった「同法」・「同令」を紐付けていく
+      let mut linked_douhou_result = find_douhou_result
+        .iter()
+        .filter_map(|l| linking_abb_and_full_name(l, &find_law_name_result))
+        .collect::<Vec<_>>();
+
+      find_law_name_result.append(&mut linked_douhou_result);
+
+      for find_law_name in find_law_name_result.iter() {
+        if let Some(l) = &find_law_name.find_law {
+          // 条項の検索をする
+          let mut to_law = l.clone();
+          let end = find_joukou(text, &find_law_name.position, &mut to_law);
+          // 結果を返す
+          result.push(Find {
+            to: to_law.clone(),
+            from: paragraph.clone(),
+            position: Position {
+              start: find_law_name.position.start,
+              end,
+            },
+          });
+        }
+      }
+
+      // 略称は他の項でも見るので追加
+      law_name_list.append(&mut linked_abb_def_result);
+    }
+    trace!(
+      "[END] parse paragraph {:?} - {:?}",
+      paragraph.article_number_str(),
+      paragraph.paragraph_number_str()
+    );
+  }
+  result
+}
+
+/// 法令名の検索
+fn find_law_name(
+  text: &str,
+  law_map: &HashMap<String, Law>,
+  find_lst: &[FindLawName],
+) -> Vec<FindLawName> {
+  let text_chars = text.chars().collect::<Vec<_>>();
+  let byte_to_char_map = byte_to_char_index_map(text);
+  let mut lst = Vec::new();
+  let mut v1 = law_map
+    .iter()
+    .map(|(k, v)| (k.clone(), v.clone()))
+    .collect::<Vec<_>>();
+  let mut v2 = find_lst
+    .iter()
+    .map(|v| (v.match_string.clone(), v.find_law.clone().unwrap()))
+    .collect::<Vec<_>>();
+  v1.append(&mut v2);
+  for (find_law_name, law) in v1.iter() {
+    for (start, s) in text.match_indices(find_law_name.as_str()) {
+      let end = start + s.len();
+      let start = byte_to_char_map[start];
+      let end = byte_to_char_map[end];
+      let match_text = s;
+      // law_nameが「法」や「令」のときは隣の文字をチェックする
+      // 隣の文字も含めて「同法」・「方法」・「法人」・「命令」、「政令」・「同令」・「法令」だった時は普遍的過ぎて法令名ではないことが多いので除外
+      // 「〇〇年法律第〇〇号」や「〇〇年〇〇省令第〇〇号」や「〇〇年〇〇委員会規則第〇〇号」や「〇〇年〇〇院規則第〇〇号」なども排除
+      let mut is_universal_name = false;
+      if *find_law_name == "法"
+        && start != 0
+        && (text_chars[start - 1] == '方'
+          || text_chars[start - 1] == '同'
+          || text_chars[start - 1] == '旧')
+      {
+        is_universal_name = true
+      }
+      if find_law_name.ends_with('法') && end < text_chars.len() - 1 && text_chars[end] == '人' {
+        is_universal_name = true
+      }
+      if *find_law_name == "法"
+        && end < text_chars.len() - 1
+        && (text_chars[end] == '令' || text_chars[end] == '律')
+      {
+        is_universal_name = true
+      }
+      if find_law_name.ends_with('法')
+        && end < text_chars.len() - 2
+        && text_chars[end] == '律'
+        && text_chars[end + 1] == '第'
+      {
+        is_universal_name = true
+      }
+      if *find_law_name == "令"
+        && start != 0
+        && (text_chars[start - 1] == '命'
+          || text_chars[start - 1] == '政'
+          || text_chars[start - 1] == '同'
+          || text_chars[start - 1] == '法'
+          || text_chars[start - 1] == '省'
+          || text_chars[start - 1] == '府'
+          || text_chars[start - 1] == '勅'
+          || text_chars[start - 1] == '旧')
+      {
+        is_universal_name = true
+      }
+      if *find_law_name == "令"
+        && end < text_chars.len() - 1
+        && text_chars[end] == '第'
+        && start != 0
+        && (text_chars[start - 1] == '省'
+          || text_chars[start - 1] == '政'
+          || text_chars[start - 1] == '勅'
+          || text_chars[start - 1] == '府')
+      {
+        is_universal_name = true
+      }
+      if find_law_name.ends_with('則')
+        && end < text_chars.len() - 1
+        && text_chars[end] == '第'
+        && start > 2
+        && (text_chars[start - 1] == '規')
+        && (text_chars[start - 2] == '院' || text_chars[start - 2] == '会')
+      {
+        is_universal_name = true
+      }
+
+      if end < text_chars.len() - 1 && text_chars[end] == '」' {
+        is_universal_name = true
+      }
+
+      if !is_universal_name {
+        let find = FindLawName {
+          position: Position { start, end },
+          match_string: match_text.to_string(),
+          find_law: Some(law.clone().clone()),
+        };
+        lst = resolve_duplicates(&lst, &find);
+      }
+    }
+  }
+  lst
+}
+
+// 略称の定義を検索
+fn find_abb_def(text: &str) -> Vec<FindLawName> {
+  let text_chars = text.chars().collect::<Vec<_>>();
+  let byte_to_char_map = byte_to_char_index_map(text);
+  let mut lst = Vec::new();
+  let abb_re = Regex::new(r"以下「([^」]*(法|令|規則))」").unwrap();
+  for m in abb_re.find_iter(text) {
+    // "以下「"の分足す
+    let start = m.start() + "以下「".len();
+    // 鉤括弧の分引く
+    let end = m.end() - "」".len();
+    // charでのインデックスにマップする
+    let start = byte_to_char_map[start];
+    let end = byte_to_char_map[end];
+    let abb = text_chars[start..end].iter().collect::<String>();
+    // 方法や命令は今回の対象ではないので除く
+    if !(abb.ends_with("方法") || abb.ends_with("命令")) {
+      let find = FindLawName {
+        position: Position { start, end },
+        match_string: abb,
+        find_law: None,
+      };
+      lst = resolve_duplicates(&lst, &find);
+    }
+  }
+  lst
+}
+
+// 「同法」と「同令」で再度検索する
+fn find_douhou(text: &str) -> Vec<FindLawName> {
+  let text_chars = text.chars().collect::<Vec<char>>();
+  let byte_to_char_map = byte_to_char_index_map(text);
+  let mut lst = Vec::new();
+  let douhou_re = Regex::new(r"同法|同令").unwrap();
+  for m in douhou_re.find_iter(text) {
+    let start = byte_to_char_map[m.start()];
+    let end = byte_to_char_map[m.end()];
+    let match_text = m.as_str();
+    if !((end < text_chars.len() - 1 && text_chars[end] == '人')
+      || (end < text_chars.len() - 2 && text_chars[end] == '律' && text_chars[end + 1] == '第'))
+    {
+      let find = FindLawName {
+        position: Position { start, end },
+        match_string: match_text.to_string(),
+        find_law: None,
+      };
+      lst.push(find);
+    }
+  }
+  lst
+}
+
+/// 条項番号を検索する
+/// 法令名の後の括弧がきを飛ばし，その後に「第一条」のような文字列が出るのを期待する
+/// "第"が出なかったら法令名だけなので処理を打ち切り
+/// 引数として可変のLawを受け取って内部の情報を更新する
+/// 返り値は最終的な範囲のend
+fn find_joukou(text: &str, position: &Position, law: &mut Law) -> usize {
+  let mut s = String::new();
+  let mut paren_depth = 0_usize;
+  let target_c = [
+    '第', '条', '項', 'の', 'ノ', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '百',
+    '千',
+  ];
+  let mut end = position.end;
+  for (i, c) in text.chars().enumerate() {
+    if i < position.end {
+      continue;
+    }
+    if c == '（' {
+      paren_depth += 1;
+      continue;
+    }
+    if c == '）' {
+      paren_depth -= 1;
+      continue;
+    }
+    if paren_depth > 0 {
+      continue;
+    }
+    if paren_depth == 0 && target_c.contains(&c) {
+      s.push(c);
+      end = i;
+      continue;
+    }
+    break;
+  }
+  // 末尾が'の', 'ノ'ならばそれを取り除く
+  if s.ends_with('の') {
+    s = s.trim_end_matches('の').to_string();
+    end -= 1;
+  }
+  if s.ends_with('ノ') {
+    s = s.trim_end_matches('ノ').to_string();
+    end -= 1;
+  }
+  trace!("find joukou number string: {s}");
+  for a in s.split("第") {
+    if !a.is_empty() {
+      let s2 = format!("第{a}");
+      trace!("find joukou number string(split): {s2}");
+      let num = parse_article_number(&s2);
+      trace!("parsed article number: {num:?}");
+      if let Some(num) = num {
+        if a.ends_with("条") {
+          law.article_number = Some(num)
+        } else if a.ends_with("項") {
+          law.paragraph_number = Some(num)
+        } else if a.ends_with("編") {
+          law.part_number = Some(num)
+        } else if a.ends_with("章") {
+          law.chapter_number = Some(num)
+        } else if a.ends_with("節") {
+          law.section_number = Some(num)
+        } else if a.ends_with("款") {
+          law.subsection_number = Some(num)
+        } else if a.ends_with("目") {
+          law.division_number = Some(num)
+        }
+      }
+    }
+  }
+  end
+}
+
+// 各charの始まりに該当するバイト位置をcharの位置に変換するためのマップ
+fn byte_to_char_index_map(text: &str) -> Vec<usize> {
+  // 各バイト位置に対する char インデックス
+  let mut map = vec![0; text.len() + 1];
+  let mut char_index = 0;
+  for (byte_index, _) in text.char_indices() {
+    map[byte_index] = char_index;
+    char_index += 1;
+  }
+  map[text.len()] = char_index;
+  map
+}
+
+/// 範囲が重複した法令について、重複を解消する
+/// 原則として範囲が大きい方が優先
+/// 同じ範囲だった場合は後から見つかったものを優先（法令名中の略称の場合なので優先される）
+/// 入力のfind_lstは常に解消済みのものであるとする
+fn resolve_duplicates(find_lst: &[FindLawName], find: &FindLawName) -> Vec<FindLawName> {
+  let mut lst = Vec::new();
+  let mut ok = false;
+  for f in find_lst.iter() {
+    if ok {
+      // 重複する物が見つかったのであとはスルー
+      lst.push(f.clone())
+    } else if f.position.start == find.position.start && f.position.end == find.position.end {
+      // 完全に一致するものが見つかったので置き換え
+      if !ok {
+        lst.push(find.clone());
+        ok = true;
+      }
+    } else if f.position.start >= find.position.start && f.position.end <= find.position.end {
+      // 見つかったものの方が小さかったので置き換え
+      if !ok {
+        lst.push(find.clone());
+        ok = true
+      }
+    } else if f.position.start <= find.position.start && f.position.end >= find.position.end {
+      // 見つかったものの方が大きかったので置き換えない
+      lst.push(f.clone());
+      ok = true
+    } else {
+      // 無関係だった
+      lst.push(f.clone())
+    }
+  }
+  // 重複は無かったので追加
+  if !ok {
+    lst.push(find.clone())
+  }
+  lst
+}
+
+// find_abb_defとsearch_douhouの実行結果と、find_law_nameの実行結果を比較して、抽出位置が直前のものを紐づける
+fn linking_abb_and_full_name(
+  abb_info: &FindLawName,
+  full_name_info_list: &[FindLawName],
+) -> Option<FindLawName> {
+  let mut result: Option<FindLawName> = None;
+  for full_name_info in full_name_info_list.iter() {
+    if full_name_info.position.end <= abb_info.position.start {
+      if let Some(ref old_result) = result
+        && full_name_info.position.end < old_result.position.end
+      {
+        // すでに見つかったものよりも遠い場合は上書きしない
+        continue;
+      } else {
+        result = Some(full_name_info.clone())
+      }
+    }
+  }
+  if let Some(result) = result {
+    Some(FindLawName {
+      find_law: result.find_law.clone(),
+      ..abb_info.clone()
+    })
+  } else {
+    None
+  }
+}
+
+#[test]
+fn check_linking() {
+  let f = FindLawName {
+    position: Position { start: 20, end: 27 },
+    match_string: String::new(),
+    find_law: None,
+  };
+  let lst = vec![
+    FindLawName {
+      position: Position { start: 5, end: 7 },
+      match_string: String::new(),
+      find_law: Some(Law::new(
+        Date::new_ad(2025, 11, 26),
+        None,
+        String::from("test1"),
+        String::new(),
+        LawType::Act,
+      )),
+    },
+    FindLawName {
+      position: Position { start: 8, end: 10 },
+      match_string: String::new(),
+      find_law: Some(Law::new(
+        Date::new_ad(2025, 11, 26),
+        None,
+        String::from("test2"),
+        String::new(),
+        LawType::Act,
+      )),
+    },
+    FindLawName {
+      position: Position { start: 29, end: 31 },
+      match_string: String::new(),
+      find_law: Some(Law::new(
+        Date::new_ad(2025, 11, 26),
+        None,
+        String::from("test3"),
+        String::new(),
+        LawType::Act,
+      )),
+    },
+  ];
+  let result = linking_abb_and_full_name(&f, &lst);
+  assert!(result.is_some());
+  assert_eq!(
+    result.unwrap().find_law.map(|l| l.get_law_id()),
+    Some(String::from("test2"))
+  );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
 use anyhow::Result;
 use clap::Parser;
+use gen_eli::law::{egov_xml_parse, parse_ref};
+use japanese_law_id::Date;
+use regex::Regex;
+use std::collections::HashMap;
+use std::path::Path;
+use tokio::fs::{self, File};
+use tokio::io::AsyncWriteExt;
+use tokio_stream::StreamExt;
+use tracing::{info, trace};
+
+async fn get_all_folder_names(path: &str) -> Result<Vec<String>> {
+  let mut dirs = tokio_stream::wrappers::ReadDirStream::new(fs::read_dir(path).await?);
+  let mut v = Vec::new();
+  while let Some(Ok(dir_entry)) = dirs.next().await {
+    if dir_entry.file_type().await?.is_dir() {
+      let s = dir_entry
+        .file_name()
+        .to_str()
+        .unwrap_or_default()
+        .to_string();
+      v.push(s)
+    }
+  }
+  Ok(v)
+}
 
 #[derive(Clone, Parser)]
 #[clap(author, version, about)]
@@ -12,7 +37,7 @@ struct Arg {
   /// e-govデータが入ったフォルダのパス
   #[clap(short, long)]
   egov_folder: String,
-  /// 出力先のフォルダ
+  /// 結果を出力するフォルダのパス
   #[clap(short, long)]
   output_folder: String,
 }
@@ -22,6 +47,131 @@ async fn run(args: Arg) -> Result<()> {
     .with_max_level(args.verbosity)
     .finish();
   tracing::subscriber::set_global_default(subscriber)?;
+
+  info!("start");
+
+  fs::create_dir_all(&args.output_folder).await?;
+
+  trace!("[START] get all folder name");
+  let folders = get_all_folder_names(&args.egov_folder).await?;
+  trace!("[END] get all folder name");
+
+  // 法令名が法令番号だけだった時に備える
+  let re_fix_name = Regex::new("^(?<name>.+号)（.+）$").unwrap();
+
+  let mut law_name_list = HashMap::new();
+  if let Ok(all_law_list_text) =
+    fs::read_to_string(format!("{}/all_law_list.csv", args.egov_folder)).await
+  {
+    let mut all_law_list_lines = all_law_list_text.lines();
+    all_law_list_lines.next();
+    for law in all_law_list_lines {
+      if !law.trim().is_empty() {
+        let info = law.split(',').collect::<Vec<_>>();
+        let law_id = info[11].to_string();
+        let mut v = Vec::new();
+        // 法令IDテキスト
+        let law_name0 = info[1];
+        if !law_name0.is_empty() {
+          v.push(law_name0.to_string());
+        }
+        // 法令名
+        let law_name1 = info[2];
+        if !law_name1.is_empty() {
+          if let Some(caps) = re_fix_name.captures(law_name1) {
+            let num_text = &caps["name"];
+            v.push(num_text.to_string());
+          }
+          v.push(law_name1.to_string());
+        }
+        // 旧法令名
+        let law_name2 = info[4];
+
+        if !law_name2.is_empty() {
+          if let Some(caps) = re_fix_name.captures(law_name2) {
+            let num_text = &caps["name"];
+            v.push(num_text.to_string());
+          }
+          v.push(law_name2.to_string());
+        }
+        law_name_list.insert(law_id.clone(), v);
+      }
+    }
+  };
+
+  let mut law_map = HashMap::new();
+  let mut target_map = HashMap::new();
+  let mut folder_stream = tokio_stream::iter(folders);
+  info!("[START] parse law files");
+  while let Some(folder_name) = folder_stream.next().await {
+    trace!("[START] parse law: {folder_name}");
+    let law_id_and_patch_id = folder_name;
+    let mut law_id = String::new();
+    let mut date_s = String::new();
+    let mut patch_id = None;
+    for (i, s) in law_id_and_patch_id.split("_").enumerate() {
+      if i == 0 {
+        law_id = s.to_string();
+      }
+      if i == 1 {
+        date_s = s.to_string();
+      }
+      if i == 2 && s != "000000000000000" {
+        patch_id = Some(s.to_string())
+      }
+    }
+    let year = date_s[0..3].parse::<usize>()?;
+    let month = date_s[4..5].parse::<usize>()?;
+    let day = date_s[5..6].parse::<usize>()?;
+    let xml_path = Path::new(&args.egov_folder)
+      .join(&law_id_and_patch_id)
+      .join(&law_id_and_patch_id)
+      .with_extension("xml");
+    let xml_file = fs::read_to_string(xml_path).await?;
+    let mut content = None;
+    if let Some(names) = law_name_list.get(&law_id) {
+      for law_name in names {
+        let (law_content, _triple) = egov_xml_parse(
+          xml_file.as_bytes(),
+          Date::new_ad(year, month, day),
+          Some(law_name.clone()),
+          law_id.clone(),
+          patch_id.clone(),
+        )?;
+        let law_info = law_content.get("").unwrap();
+        law_map.insert(law_name.clone(), law_info.clone());
+        content = Some(law_content);
+      }
+    }
+    if let Some(c) = content {
+      target_map.insert(law_id_and_patch_id, c);
+    }
+  }
+  info!("[END] parse law files");
+
+  let mut target_stream = tokio_stream::iter(target_map);
+
+  info!("[START] analysis");
+  while let Some((id, target)) = target_stream.next().await {
+    trace!("[START] analysis: {id}",);
+    let finds = parse_ref(&target, &law_map);
+    if !finds.is_empty() {
+      trace!("[START] write: {id}");
+      let output_file_path = Path::new(&args.output_folder)
+        .join(&id)
+        .with_extension("jsonl");
+      let mut output_file = File::create(output_file_path).await?;
+      let mut find_stream = tokio_stream::iter(finds);
+      while let Some(result) = find_stream.next().await {
+        let s = serde_json::to_string(&result)?;
+        output_file.write_all(format!("{s}\n").as_bytes()).await?;
+      }
+      output_file.flush().await?;
+      trace!("[END] write: {id}");
+    }
+    trace!("[END] analysis: {id}",);
+  }
+  info!("[END] analysis");
 
   Ok(())
 }
